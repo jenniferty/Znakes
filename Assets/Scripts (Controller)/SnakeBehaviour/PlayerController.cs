869ef31e285129bc91287f6b8b0f1e2038f43ce3
An1honyY <ant.jo.yuen@gmail.com>
2022-09-19 16:57:06 +1200

    broken Position History code
    commit is just to save progress. It's not working

--- Assets/Scripts (Controller)/SnakeBehaviour/PlayerController.cs
+++ Assets/Scripts (Controller)/SnakeBehaviour/PlayerController.cs
@@ -9,11 +9,20 @@ public class PlayerController : MonoBehaviour
     // Settings
     public float speed = 5;
     public float rotationSpeed = 180;
+    public int gap = 10;
+
+    public GameObject BodyPrefab;
+
+    private List<GameObject> BodyParts = new List<GameObject>();
+    private List<Vector3> PositionsHistory = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
     {
-
+        for (int i = 0; i < 5; i++)
+        {
+            GrowSnake();
+        }
     }
 
     // Update is called once per frame
@@ -26,6 +35,23 @@ public class PlayerController : MonoBehaviour
         float direction = Input.GetAxis("Horizontal"); 
         transform.Rotate(Vector3.up * direction * rotationSpeed * Time.deltaTime);
 
+        // Storing Position
+        PositionsHistory.Insert(0, transform.position);
+
+        // Moving bodyparts
+        int index = 0;
+        foreach (var body in BodyParts)
+        {
+            // point is the latest position in history 
+            Vector3 point = PositionsHistory[Mathf.Min(index * gap, PositionsHistory.Count-1)];
+            index++;
+        }
+    }
+
+    private void GrowSnake()
+    {
+        GameObject Cube_body = Instantiate(BodyPrefab);
+        BodyParts.Add(Cube_body);
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit)
