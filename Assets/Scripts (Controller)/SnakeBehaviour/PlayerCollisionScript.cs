76376d88e862c42116c95160eceacc6d688a7450
Dom <gsr1584@autuni.ac.nz>
2022-09-22 17:28:14 +1200

    Added growth+food Spawn
    Food spawns and the snake grows

---
+++ Assets/Scripts (Controller)/SnakeBehaviour/PlayerCollisionScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionScript : MonoBehaviour
+{
+    public FoodSpawnerScript foodSpawner;
+    public PlayerController playerSnake;
+    // PlayerController snakePlayer = new PlayerController();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("collision ");
+        //  snakePlayer.GrowSnake();
+
+        if (collider.CompareTag("Food"))
+        {
+
+            foodSpawner.Spawn();
+            Destroy(collider.gameObject);
+            playerSnake.GrowSnake();
+        }
+
+        if (collider.CompareTag("wall"))
+        {
+            Destroy(playerSnake);
+            Debug.Log("Collions Wall");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts (Controller)/SnakeBehaviour/PlayerController.cs
+++ Assets/Scripts (Controller)/SnakeBehaviour/PlayerController.cs
@@ -9,32 +9,61 @@ public class PlayerController : MonoBehaviour
     // Settings
     public int health = 100;
     public int maxHealth = 100;
-    public float speed = 5;
-    public float rotationSpeed = 180;
+    //public float speed = 5;
+    //public float rotationSpeed = 180;
+    public float MoveSpeed = 5;
+    public float SteerSpeed = 180;
+    public float bodySpeed = 5;
+    public int Gap = 20;
 
     Camera attachedCam;
     public HealthBar healthBar;
     public LoseScreen loseScreen;
+
+    //code for growth function
+    public GameObject snakeBody;
+    private List<GameObject> bodyParts = new List<GameObject>();
+    private List<Vector3> positionHist = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
     {
         attachedCam = GameObject.Find("Third Person Camera").GetComponent<Camera>();
         healthBar.SetHealth(health);
+
+        GrowSnake();
     }
 
     // Update is called once per frame
     void Update()
     {
         // For constant forward movement
-        controller.Move(transform.forward * speed * Time.deltaTime);
+        //controller.Move(transform.forward * speed * Time.deltaTime);
 
         // Rotation with arrow keys
-        float direction = Input.GetAxis("Horizontal"); 
-        transform.Rotate(Vector3.up * direction * rotationSpeed * Time.deltaTime);
+        // float direction = Input.GetAxis("Horizontal"); 
+        // transform.Rotate(Vector3.up * direction * rotationSpeed * Time.deltaTime);
+
+        //new controle movement
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        float steerDirection = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * steerDirection * SteerSpeed * Time.deltaTime);
 
         // Health and Death
         CheckDeath();
+
+        positionHist.Insert(0, transform.position);
+
+        int index = 0;
+        foreach (var body in bodyParts)
+        {
+            Vector3 point = positionHist[Mathf.Min(index * Gap, positionHist.Count - 1)];
+            Vector3 moveDirection = point - body.transform.position;
+            body.transform.position += moveDirection * bodySpeed * Time.deltaTime;
+            body.transform.LookAt(point);
+            index++;
+        }
+
     }
 
     //todo
@@ -61,4 +90,11 @@ public class PlayerController : MonoBehaviour
             loseScreen.ShowDeathScreen();
         }
     }
+
+    public void GrowSnake()
+    {
+        GameObject body = Instantiate(snakeBody);
+        bodyParts.Add(body);
+    }
+
 }
