59c213c653825b25ca7d8e3c6f88d2d1fef63e5b
Dom <dominicphilippu@gmail.com>
2022-09-23 14:24:22 +1200

    Have implemented growth function
    The snake grows when eating yellow pallets. There is also a player collision script we can use for for wall and obstacles

--- Assets/Scripts (Controller)/SnakeBehaviour/PlayerController.cs
+++ Assets/Scripts (Controller)/SnakeBehaviour/PlayerController.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour 
 {
-    public CharacterController controller;
-//test for git
+    
+
     // Settings
     public int health = 100;
     public int maxHealth = 100;
-    public float speed = 5;
-    public float rotationSpeed = 180;
+    public float MoveSpeed = 5;
+    public float SteerSpeed = 180;
+    public float bodySpeed = 5;
+    public int Gap = 20;
+
 
     Camera attachedCam;
     public HealthBar healthBar;
     public LoseScreen loseScreen;
+
+    //for the snake Tail/Growth
+    public GameObject snakeBody;
+    private List<GameObject> bodyParts = new List<GameObject>();
+    private List<Vector3> positionHist = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
@@ -26,12 +34,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // For constant forward movement
-        controller.Move(transform.forward * speed * Time.deltaTime);
+        //new movementcode
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
 
-        // Rotation with arrow keys
-        float direction = Input.GetAxis("Horizontal"); 
-        transform.Rotate(Vector3.up * direction * rotationSpeed * Time.deltaTime);
+
+        float steerDirection = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * steerDirection * SteerSpeed * Time.deltaTime);
+
+
+        //new growth function code
+        positionHist.Insert(0, transform.position);
+
+        //set to 1 so the head does not collide with the snake in the beginning
+        int index = 1;
+        foreach (var body in bodyParts)
+        {
+            Vector3 point = positionHist[Mathf.Min(index * Gap, positionHist.Count - 1)];
+            Vector3 moveDirection = point - body.transform.position;
+            body.transform.position += moveDirection * bodySpeed * Time.deltaTime;
+            body.transform.LookAt(point);
+            index++;
+        }
 
         // Health and Death
         CheckDeath();
@@ -60,5 +83,10 @@ public class PlayerController : MonoBehaviour
             PauseGame.Pause();
             loseScreen.ShowDeathScreen();
         }
+    }
+    public void GrowSnake()
+    {
+        GameObject body = Instantiate(snakeBody);
+        bodyParts.Add(body);
     }
 }
