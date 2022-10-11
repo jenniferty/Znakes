266483f4608caea4f7d20ec1ded0f85e11e3fb2d
jenniferty <bqd3000@autuni.ac.nz>
2022-10-12 12:36:27 +1300

    version 1 complete

---
+++ Assets/Scripts (Controller)/UIScripts/Minimap.cs
@@ -0,0 +1,23 @@
+/*
+Code copied from https://weeklyhow.com/unity-minimap-in-10-minutes/
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    private Transform _player;
+
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 playerPosition = _player.position;
+        playerPosition.y = transform.position.y;
+        transform.position = playerPosition;
+    }
+}
