36138ccf6c2325a075f72bb5cc24a3e3d531db94
jenniferty <bqd3000@autuni.ac.nz>
2022-10-12 21:16:57 +1300

    create script to return the boundaries of each map

---
+++ Assets/Scripts (Controller)/EnemyScripts/EnemySpawnLocation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnLocation : MonoBehaviour
+{
+    private int mapIndex;
+    public float[,] coordinates;
+    void Start(){
+        
+    }
+
+    public float[,] getCoordinates()
+    {
+        switch(getMapIndex())
+        {
+            case 0:
+            return coordinates = new float[,]{{0f, 0f, 0f, 0f, 0f},{-25f, 25f, -25f, 25f, 3.8f}};
+            case 1:
+            return coordinates = new float[,]{{0f, 0f, 0f, 0f, 0f},{-25f, 25f, -25f, 25f, 3.8f}};
+            case 2:
+            return coordinates = new float[,]{{0f, 0f, 0f, 0f, 0f},{-25f, 25f, -25f, 25f, 3.8f}};
+            case 3:
+            return coordinates = new float[,]{{0f, 0f, 0f, 0f, 0f},{-25f, 25f, -25f, 25f, 3.8f}};
+            case 4:
+            return coordinates = new float[,]{{0f, 0f, 0f, 0f, 0f},{-25f, 25f, -25f, 25f, 3.8f}};
+            default:
+            return coordinates = new float[,]{{0f, 0f, 0f, 0f, 0f},{-25f, 25f, -25f, 25f, 3.8f}};
+        }
+    }
+    public float getMapIndex()
+    {
+        return this.mapIndex;
+    }
+    public void setMapIndex(int mapIndex)
+    {
+        this.mapIndex = mapIndex;
+    }
+}
