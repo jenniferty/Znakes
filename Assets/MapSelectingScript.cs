511ece1082fa83c040bc416aba1c13886638693a
lilzhuo <zcy1226296762@gmail.com>
2022-10-18 21:50:12 +1300

    updatedmenu18/10

--- Assets/MapSelectingScript.cs
+++ Assets/MapSelectingScript.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MapSelectingScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Map1 ()
     {
-        
+        SceneManager.LoadScene("Map1");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Map2 ()
     {
-        
+        SceneManager.LoadScene("Map2");
     }
+
+    public void Map3 ()
+    {
+        SceneManager.LoadScene("Map3");
+    }
+
+    
+
 }
