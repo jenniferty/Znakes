02785423181bb61e562973c1b75ebff292b03a43
Dom <dominicphilippu@gmail.com>
2022-10-22 11:16:16 +1300

    added backend code
    score increments by 50 when eating the yellow ball.
    I will add other food types later on. The score works

--- Assets/Scripts (Controller)/SnakeBehaviour/PlayerController.cs
+++ Assets/Scripts (Controller)/SnakeBehaviour/PlayerController.cs
@@ -28,7 +28,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //new movementcode
         transform.position += transform.forward * moveSpeed * Time.deltaTime;
--- Assets/Scripts (Controller)/UIScripts/Scoring System/ScoreCounter.cs
+++ Assets/Scripts (Controller)/UIScripts/Scoring System/ScoreCounter.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+//script which holds score
 public class ScoreCounter : MonoBehaviour
 {
-    int score;
+    public static int score;
     TextMeshProUGUI scoreText;
     // Start is called before the first frame update
-    void Start()
+   void Start()
     {
         scoreText = GetComponentInChildren<TextMeshProUGUI>();
         score = 0;
     }
+   
 
+    private void OnTriggerEnter(Collider other)
+    {
+        score += 50;
+        scoreText.SetText("Score: " + score);
+        Destroy(gameObject);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        score += 1;
+       // score += 1;
         scoreText.SetText("Score: " + score); 
     }
+   
 }
