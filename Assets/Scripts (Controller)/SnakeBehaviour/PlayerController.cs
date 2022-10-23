8232d934ec9f4d62e2a7edc5bc9b5cfaeac19e24
An1honyY <ant.jo.yuen@gmail.com>
2022-10-24 11:46:55 +1300

    Basic Stamina logic, still need to connect logic to the bar.
    Perhaps add a stamia bar script

--- Assets/Scripts (Controller)/SnakeBehaviour/PlayerController.cs
+++ Assets/Scripts (Controller)/SnakeBehaviour/PlayerController.cs
@@ -11,15 +11,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int gap = 15;
     private int bodyCount = 0;
     private bool speedPowerupIsActive = false;
+    private bool isSprinting = false;
+    private float stamina = 3f;
+    private float staminaMax = 3f;
+    private float staminaDecreasePerFrame = 0.2f;
+    private float staminaIncreasePerFrame = 0.2f;
+    private float staminaRegenTimer = 0f;
+    private float staminaTimeToRegen = 3f;
     Camera attachedCam;
 
     //for the snake Tail/Growth
     public GameObject snakeBody;
     private List<GameObject> bodyParts = new List<GameObject>();
     private List<Vector3> positionHist = new List<Vector3>();
-
-    public bool isSprinting = false;
-    // public float sprintMultiplier;
 
     // Start is called before the first frame update
     void Start()
@@ -40,23 +44,14 @@ public class PlayerController : MonoBehaviour
         {
             ShiftSprint();
         }
+        
+        UpdateStamina();
+
         //new movementcode
         transform.position += transform.forward * moveSpeed * Time.deltaTime;
 
         float steerDirection = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up * steerDirection * steerSpeed * Time.deltaTime);
-
-        //sprint functions, changeing speed
-        // if (isSprinting == true) 
-        // {
-        //     MoveSpeed *= sprintMultiplier;
-        //     // bodySpeed *= sprintMultiplier;
-        // }
-        // else
-        // {
-        //     MoveSpeed = 5;
-        //     // bodySpeed = 5;
-        // }
 
         if (!PauseGame.isPaused)
         {
@@ -85,25 +80,37 @@ public class PlayerController : MonoBehaviour
 
     public void ShiftSprint()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        isSprinting = Input.GetKey(KeyCode.LeftShift);
+        if (isSprinting)
         {
-            /*MoveSpeed = 10;
-            bodySpeed = 10;
-            Gap = 5;*/
             setMoveSpeed(10f);
             setBodySpeed(10f);
             setGap(5);
         }
         else
         {
-            /*MoveSpeed = 5;
-            bodySpeed = 5;
-            Gap = 10;*/
             setMoveSpeed(5f);
             setBodySpeed(5f);
             setGap(10);
         }
     }
+
+    public void UpdateStamina()
+    {
+        if (isSprinting)
+        {
+            stamina = Mathf.Clamp(stamina - (staminaDecreasePerFrame * Time.deltaTime), 0.0f, staminaMax);
+            staminaRegenTimer = 0.0f;
+        }
+        else if (stamina < staminaMax)
+        {
+            if (staminaRegenTimer >= staminaTimeToRegen)
+                stamina = Mathf.Clamp(stamina + (staminaIncreasePerFrame * Time.deltaTime), 0.0f, staminaMax);
+            else
+                staminaRegenTimer += Time.deltaTime;
+        }
+    }
+
     public int getBodyCount()
     {
         return bodyCount;
