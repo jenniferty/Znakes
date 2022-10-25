1eeb24c629f2a8b5ce7f4bc7b77371e2aa448d26
lilzhuo <90330271+lilzhuo@users.noreply.github.com>
2022-10-26 00:02:26 +1300

    updateMapSelectingScript

--- Assets/Scripts/MenuScripts/MapSelectingScript.cs
+++ Assets/Scripts/MenuScripts/MapSelectingScript.cs
@@ -15,6 +15,11 @@ public class MapSelectingScript : MonoBehaviour
         location = FindObjectOfType<ObjectSpawnLocation>();
         playerHealth = FindObjectOfType<PlayerHealth>();
     }
+    public void Story()
+    {
+        audioManager.playGameStopMenuTheme();
+        SceneChanger.LoadScene("Story");
+    }
     public void Map1 ()
     {
         audioManager.playGameStopMenuTheme();
@@ -37,5 +42,19 @@ public class MapSelectingScript : MonoBehaviour
         location.setMapIndex(3);
         playerHealth.setSnakeHealth(playerHealth.getSnakeMaxHealth());
         SceneChanger.LoadScene("Map3");
+    }
+    public void Map4 ()
+    {
+        audioManager.playGameStopMenuTheme();
+        location.setMapIndex(4);
+        playerHealth.setSnakeHealth(playerHealth.getSnakeMaxHealth());
+        SceneChanger.LoadScene("Map4");
+    }
+    public void Ma5 ()
+    {
+        audioManager.playGameStopMenuTheme();
+        location.setMapIndex(5);
+        playerHealth.setSnakeHealth(playerHealth.getSnakeMaxHealth());
+        SceneChanger.LoadScene("Map5");
     }
 }
