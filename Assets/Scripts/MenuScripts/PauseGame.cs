89ac6732b48de2312733e35ffa78e5b1f0044fa3
lilzhuo <90330271+lilzhuo@users.noreply.github.com>
2022-10-25 13:35:28 +1300

    UpdatedPauseMenu

--- Assets/Scripts/MenuScripts/PauseGame.cs
+++ Assets/Scripts/MenuScripts/PauseGame.cs
@@ -2,47 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PauseGame : MonoBehaviour
 {
     public static bool isPaused = false;
 
-    public PlayerController playerController;
+    //public PlayerController playerController;
 
-    [SerializeField] GameObject PauseMenu;
+    [SerializeField]GameObject PauseMenu;
 
-    // Start is called before the first frame update
+
+
+    /*Start is called before the first frame update
     void Start()
     {
         
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
         {
-            if (!isPaused)
+            if (isPaused)
             {
-                Pause();
+                Resume();
+                
             }
             else
             {
-                Resume();
+                Pause();
             }
         }
     }
 
     public void Pause()
     {
-        Time.timeScale = 0;
-        isPaused = true;
         PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        //Cursor.lockState = CursorLockMode.Confined;
+       
+
     }
 
     public void Resume()
     {
-        Time.timeScale = 1;
-        isPaused = false;
         PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        Debug.Log("clicked");
+     
+
+    }
+
+    public void MenuButton()
+    {
+        SceneChanger.LoadScene("MainMenu");
+        Debug.Log("Go to menus");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
     }
 }
